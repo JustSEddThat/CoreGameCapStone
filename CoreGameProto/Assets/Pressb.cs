d1e2e7116808efc0af9358a262b02326c66b267f
JustSEddThat <edd_brown@bloomfield.edu>
2017-12-07 14:04:50 -0500

    Updated scenes and plays

---
+++ CoreGameProto/Assets/Pressb.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pressb : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Fire3"))
+			GoBack ();
+	}
+
+	public void GoBack()
+	{
+		SceneManager.LoadScene ("Title Screen");
+	}
+}
